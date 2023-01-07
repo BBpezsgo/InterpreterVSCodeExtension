5098f545d5605df2cbc79f41c4135af9ee6fb988
BB pezsgő <bazsi5fel2@gmail.com>
2023-01-07 13:10:23 +0100

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,6 +1,10 @@
+chcp 65001
 @echo off
-set from=".\BBCodeExtension.vsix"
-set to="C:\Users\bazsi\OneDrive\Download Content"
-echo Copy %from% to %to% ...
-copy %from% %to%
+set source=".\BBCodeExtension.vsix"
+set onedrive="C:\Users\bazsi\OneDrive\Download Content"
+set googledrive="C:\Users\bazsi\Saját meghajtó\Download Content"
+echo Copy to OneDrive ...
+copy %source% %onedrive%
+echo Copy to Google Drive ...
+copy %source% %googledrive%
 echo Done
